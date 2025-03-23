20cdd714f843202ab83ca660db5b3ebb499e6330
YieldRay <yieldray@outlook.com>
2025-03-23 15:09:45 +0800

    add license plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,21 @@ import { execSync } from "node:child_process";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import license from "rollup-plugin-license";
 import { dependencies, devDependencies } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "./",
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        license({
+            thirdParty: {
+                output: "./dist/assets/vendor.LICENSE.txt",
+            },
+        }),
+    ],
     define: {
         __HEAD__: JSON.stringify(execSync("git rev-parse HEAD", { encoding: "utf8" })),
         __DEPS__: JSON.stringify({ dependencies, devDependencies }),
@@ -20,5 +29,7 @@ export default defineConfig({
     },
     esbuild: {
         drop: ["console", "debugger"],
+        banner: "/*! licenses: /assets/vendor.LICENSE.txt */",
+        legalComments: "none",
     },
 });
